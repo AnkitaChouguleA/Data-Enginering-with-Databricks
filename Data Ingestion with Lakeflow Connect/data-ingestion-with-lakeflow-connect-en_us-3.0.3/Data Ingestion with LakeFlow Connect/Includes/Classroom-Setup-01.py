78a023db4370e8235a1b7764ddd01b4cbe4ae668
AnkitaChouguleA <ankitanavalgunde161999@gmail.com>
2026-05-18 09:48:22 +0000

    Second commit

---
+++ Data Ingestion with Lakeflow Connect/data-ingestion-with-lakeflow-connect-en_us-3.0.3/Data Ingestion with LakeFlow Connect/Includes/Classroom-Setup-01.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "2"
+# ///
+# MAGIC %run ./Classroom-Setup-Common
+
+# COMMAND ----------
+
+spark.sql('DROP TABLE IF EXISTS mydeltatable')
+spark.sql('CREATE TABLE IF NOT EXISTS mydeltatable (id INT, name STRING)')
+spark.sql('INSERT INTO mydeltatable (id, name) VALUES (1,"Peter")')
+print('Created Delta table mydeltatable for the demonstration.')
---
+++ Data Ingestion with Lakeflow Connect/data-ingestion-with-lakeflow-connect-en_us-3.0.3/Data Ingestion with LakeFlow Connect/Includes/Classroom-Setup-06.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "2"
+# ///
+# MAGIC %run ./Classroom-Setup-Common
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC USE CATALOG dbacademy;
+# MAGIC USE SCHEMA IDENTIFIER(DA.schema_name);
